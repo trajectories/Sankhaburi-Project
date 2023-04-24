0e676e35c85ccf1ab7aa4ff6d24cd660a02a1112
trajectories <79078599+trajectories@users.noreply.github.com>
2023-04-24 16:29:57 +0700

    admin

--- admin/services/change_password.php
+++ admin/services/change_password.php
@@ -1,59 +1,39 @@
 <?php
-session_start();
+// Replace with your database credentials
+include "../db/db.php";
 
-// Check if the user is logged in
-if (!isset($_SESSION['user_id'])) {
-  // Redirect to login page if not logged in
-  header("Location: login.php");
+// Get form data
+$current_password = $_POST['current-password'];
+$new_password = $_POST['new-password'];
+$confirm_password = $_POST['confirm-password'];
+
+// Check if new password matches confirm password
+if ($new_password != $confirm_password) {
+  echo "error";
   exit();
 }
 
-// Check if the form was submitted
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  // Get the current password, new password, and confirm password from the form
-  $current_password = $_POST['current-password'];
-  $new_password = $_POST['new-password'];
-  $confirm_password = $_POST['confirm-password'];
+// Get user ID from session or cookie
+$user_id = 1;
 
-  // Validate the new password and confirm password
-  if ($new_password !== $confirm_password) {
-    echo 'error';
-    exit();
-  }
+// Check if current password is correct
+$sql = "SELECT * FROM users WHERE id='$user_id' AND password='$current_password'";
+$result = mysqli_query($conn, $sql);
 
-  // Get the user's ID from the session
-  $user_id = $_SESSION['user_id'];
-
-  // Connect to the database
-include "../db/db.php";
-
-  // Prepare the SQL statement to get the user's current password hash
-  $stmt = $mysqli->prepare("SELECT password FROM users WHERE id = ?");
-  $stmt->bind_param("i", $user_id);
-  $stmt->execute();
-  $stmt->bind_result($password_hash);
-  $stmt->fetch();
-  $stmt->close();
-
-  // Verify the current password
-  if (!password_verify($current_password, $password_hash)) {
-    echo 'error';
-    exit();
-  }
-
-  // Prepare the SQL statement to update the user's password hash
-  $stmt = $mysqli->prepare("UPDATE users SET password = ? WHERE id = ?");
-  $new_password_hash = password_hash($new_password, PASSWORD_DEFAULT);
-  $stmt->bind_param("si", $new_password_hash, $user_id);
-  $stmt->execute();
-
-  // Check if the update was successful
-  if ($stmt->affected_rows === 1) {
-    echo 'success';
+if (mysqli_num_rows($result) == 1) {
+  // Update password in database
+  $sql = "UPDATE users SET password='$new_password' WHERE id='$user_id'";
+  if (mysqli_query($conn, $sql)) {
+    echo "success";
   } else {
-    echo 'error';
+    echo "error";
   }
-
-  // Close the database connection
-  $mysqli->close();
+} else {
+  // Current password is invalid
+  echo "invalid";
 }
+
+// Close connection
+mysqli_close($conn);
+
+?>
